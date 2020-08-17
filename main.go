8cb169bf0a591f8f5e48f8690c6b5728c3d04107
Eze Olea Figueroa <ezeoleaf@gmail.com>
2020-08-16 21:01:04 -0300

    Adding models

--- main.go
+++ main.go
@@ -6,27 +6,51 @@ import (
 	tm "github.com/buger/goterm"
 )
 
+type Conway struct {
+	Size   int
+	State  [][]string
+	Living int
+	Dead   int
+}
+
 func main() {
 	tm.Clear() // Clear current screen
+
+	c := Conway{Size: 10}
+
+	c.born()
 
 	for {
 		// By moving cursor to top-left position we ensure that console output
 		// will be overwritten each time, instead of adding new.
 		tm.MoveCursor(1, 1)
 
-		rows := 10
-		cols := 10
-
-		for i := 1; i <= rows; i++ {
-			row := ""
-			for j := 1; j <= cols; j++ {
-				row += "- "
-			}
-			tm.Println(row)
-		}
+		c.show()
 
 		tm.Flush() // Call it every time at the end of rendering
 
 		time.Sleep(time.Second)
+	}
+}
+
+func (c Conway) born() {
+	for i := 0; i < c.Size; i++ {
+		for j := 0; j < c.Size; j++ {
+			c.State[i][j] = " "
+		}
+	}
+}
+
+func live() {
+
+}
+
+func (c Conway) show() {
+	for i := 0; i < c.Size; i++ {
+		row := ""
+		for j := 0; j < c.Size; j++ {
+			row += c.State[i][j]
+		}
+		tm.Println(row)
 	}
 }
