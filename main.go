41762cec3fe63265ced3d2dd61557365e909b713
Eze Olea Figueroa <ezeoleaf@gmail.com>
2020-08-22 21:44:18 -0300

    Added live function

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 
 	tm "github.com/buger/goterm"
 )
+
+const cell = "x"
+const space = " "
 
 type Conway struct {
 	Size   int
@@ -26,12 +30,16 @@ func main() {
 		show(c)
 		tm.Flush() // Call it every time at the end of rendering
 
+		c = live(c)
+
 		time.Sleep(time.Second)
 	}
 }
 
 func born() Conway {
 	c := Conway{Size: 10}
+
+	rc := getRandomCells(5, c.Size)
 
 	c.State = make([][]string, c.Size)
 	for i := 0; i < c.Size; i++ {
@@ -40,32 +48,83 @@ func born() Conway {
 
 	for i, innerArray := range c.State {
 		for j := range innerArray {
-			c.State[i][j] = " "
+			s := string(i) + "-" + string(j)
+			if isIn(rc, s) {
+				c.State[i][j] = cell
+			} else {
+				c.State[i][j] = space
+			}
 		}
 	}
 
 	return c
 }
 
-func live() {
+func getRandomCells(q int, s int) []string {
+	rc := make([]string, q)
+	for i := 0; i < q; i++ {
+		for {
+			r1 := rand.Intn(s)
+			r2 := rand.Intn(s)
+			s := string(r1) + "-" + string(r2)
+			if !isIn(rc, s) {
+				rc = append(rc, s)
+				break
+			}
+		}
+	}
+	return rc
+}
 
+func isIn(s []string, v string) bool {
+	for _, val := range s {
+		if val == v {
+			return true
+		}
+	}
+	return false
+}
+
+func live(c Conway) Conway {
+	for i, innerArray := range c.State {
+		for j := range innerArray {
+			cl := c.State[i][j]
+			lc := getLivingCells(c, i, j)
+			if cl == cell {
+
+			} else {
+
+			}
+		}
+	}
+
+	return c
+}
+
+func getLivingCells(c Conway, i int, j int) int {
+	livingCells := 0
+
+	for k := -1; k <= 1; k++ {
+		for l := -1; l <= 1; l++ {
+			ni := i + k
+			nj := j + l
+
+			if (ni < 0 || nj < 0) || (ni > c.Size - 1 || nj > c.Size - 1) {
+				continue
+			}
+			fmt.Println(ni, nj)
+		}
+	}
+
+	return livingCells
 }
 
 func show(c Conway) {
-	// for i := 0; i < c.Size; i++ {
-	// 	row := ""
-	// 	for j := 0; j < c.Size; j++ {
-	// 		row += c.State[i][j]
-	// 	}
-	// 	tm.Println(row)
-	// }
-	// fmt.Println(c.State)
 	for i, innerArray := range c.State {
 		row := ""
 		for j := range innerArray {
 			row += c.State[i][j] + " "
 		}
-		fmt.Println(row)
-		// tm.Println(row)
+		tm.Println(row)
 	}
 }
